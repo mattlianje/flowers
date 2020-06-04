9f1be2c8ca9aad2589b7354b04e43c0b2e897993
Matthieu Court <matthieu.court@gmail.com>
2020-06-04 02:03:10 -0400

    implemented testing wheels as on Sz40/42

--- lorenz/src/main/scala/lorenz/Run.scala
+++ lorenz/src/main/scala/lorenz/Run.scala
@@ -5,10 +5,12 @@ import monocle.macros.Lenses
 
 
 object Main extends App {
-  println("test")
 
   val test = Baudot.XOR("10010", "00001", 0)
-
+  val test2 = Baudot.charToBaudot("C")
+  println(test2)
+  val testWheel = Wheels.chi_I(0).getRandomPins(15, List())
+  for(element <- testWheel) {println(element)}
   // Example case classes ...
   @Lenses case class Address(street: String, city: String, zip: Int)
   @Lenses case class Person(name: String, age:Int, address: Address)
--- lorenz/src/main/scala/lorenz/Wheel.scala
+++ lorenz/src/main/scala/lorenz/Wheel.scala
@@ -1,13 +1,104 @@
 package lorenz
+import scala.util.Random
 
 case class Wheel(
-                // Indices of which pins are flipped on the wheel to register as a binary '1'.
-                pin_settings: String,
-                // As per Bill Tutte's nomenclature, you had χ, ψ, and μ wheels - to be implemented as chi, psi and mu.
+                // pin_settings: Indices of which pins are flipped on the wheel to register as a binary '1'.
+                pin_settings: List[Int],
+                // wheel_type: As per Bill Tutte's nomenclature, you had χ, ψ, and μ wheels - to be implemented as chi, psi and mu.
                 wheel_type: String,
                 number_of_pins: Int,
-                // Current pin the wheel is at.
+                // position: Current pin the wheel is at.
                 position: Int
                 ) {
 
+  val random = new Random
+  def getRandomPins (numPins: Int, curr: List[Int]): List[Int] = {
+    val possiblePinSettings = Seq(1, 0)
+    val randomPin = possiblePinSettings(random.nextInt(possiblePinSettings.length))
+    val newList = curr ++ List(randomPin)
+    if (newList.length == numPins) {
+      newList
+    }
+    else {
+      getRandomPins(numPins, newList)
+    }
+  }
+}
+
+// https://cs.stanford.edu/people/eroberts/courses/soco/projects/2008-09/colossus/lorenzmachine.html
+// Some fixed pin settings for testing purposes
+
+object Wheels {
+  def chi_I (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1),
+    wheel_type = "chi_I",
+    number_of_pins = 41,
+    position = p
+  )
+  def chi_II (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1),
+    wheel_type = "chi_II",
+    number_of_pins = 31,
+    position = p
+  )
+  def chi_III (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0),
+    wheel_type = "chi_III",
+    number_of_pins = 29,
+    position = p
+  )
+  def chi_IV (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1),
+    wheel_type = "chi_IV",
+    number_of_pins = 26,
+    position = p
+  )
+  def chi_V (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1),
+    wheel_type = "chi_V",
+    number_of_pins = 23,
+    position = p
+  )
+  def psi_I (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1),
+    wheel_type = "psi_I",
+    number_of_pins = 43,
+    position = p
+  )
+  def psi_II (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0),
+    wheel_type = "psi_II",
+    number_of_pins = 47,
+    position = p
+  )
+  def psi_III (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,0,0,1),
+    wheel_type = "psi_III",
+    number_of_pins = 51,
+    position = p
+  )
+  def psi_IV (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,0,0,1,0,0),
+    wheel_type = "psi_IV",
+    number_of_pins = 53,
+    position = p
+  )
+  def psi_V (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1),
+    wheel_type = "psi_V",
+    number_of_pins = 59,
+    position = p
+  )
+  def mu_I (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0),
+    wheel_type = "mu_I",
+    number_of_pins = 61,
+    position = p
+  )
+  def mu_II (p: Int) = Wheel(
+    pin_settings = List(1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0),
+    wheel_type = "mu_II",
+    number_of_pins = 37,
+    position = p
+  )
 }
